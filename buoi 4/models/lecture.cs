6887351a990743f0237d65e2982b4fa2b3ca36ce
Lytanloc <lyloc14112003@gmail.com>
2023-12-13 13:55:17 +0700

    Create Lecture.cs in folder models

---
+++ buoi 4/models/lecture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi_4.models
+{
+    internal class lecture
+    {
+    }
+}
